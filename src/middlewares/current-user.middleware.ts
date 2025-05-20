d14e8300e51108c64e220c80bc3d237a65290fa4
mdop297 <lenhatminh297@gmail.com>
2025-05-20 11:30:36 +0700

    fix type error

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/users.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -11,8 +20,8 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.userService.findOne(userId);
-      //@ts-ignore
-      req.currentUser = user;
+
+      req.currentUser = user ?? undefined;
     }
 
     next();
